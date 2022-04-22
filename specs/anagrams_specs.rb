5536c6e973d5446d089c59a49809dd6877513c47
Marcus <marcusferreira23@gmail.com>
2022-04-22 13:42:23 -0700

    removed incorrect specs directory

--- specs/anagrams_specs.rb
+++
@@ -1,9 +0,0 @@
-require ('rspec')
-require ('anagrams')
-
-describe("String#anagram") do
-  it("recognizes if two words are anagrams") do
-    string = String.new
-    expect(string.anagram("ruby", "bury")).to(eq("These words are anagrams."))
-  end
-end
